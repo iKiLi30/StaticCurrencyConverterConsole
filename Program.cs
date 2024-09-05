f4193e18ad85f4fc7bc41eaf0785a40598287881
user <user@WIN-HIJ4FH2GJF8>
2024-09-05 10:54:47 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StaticCurrencyConverterConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! Its a simple currency converter, convert YOUR currency in MY currency");
+            Console.WriteLine("Input number and press Enter");
+            Program program = new Program();
+            program.Converter();
+
+        }
+        void Converter()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int myNumber = number * 2;
+            Console.WriteLine($"By our exchange rate {number} YOUR currency is {myNumber} MY currency");
+        }
+    }
+}
